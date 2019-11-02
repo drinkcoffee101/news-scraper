ef6a79a5865f81546a794873edfb3ab15765f729
drinkcoffee <dcoffie92@gmail.com>
2019-11-01 20:46:45 -0700

    added delete route

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
@@ -12,29 +11,45 @@ router.get('/', (req, res) => {
 })
 //get route to scrape website for information and store in db
 router.get('/scrape', (req, res) => {
-    axios.get('https://www.attackmagazine.com/news/').then((result) => {
-        const html = result.data
-        const $ = cheerio.load(html)
-        //search for each element containing article 
-        $('.article--wide').each((i, element) => {
+    axios.get('https://www.attackmagazine.com/news/')
+        .then((result) => {
+            const html = result.data
+            const $ = cheerio.load(html)
+            //search for each element containing article 
+            $('.article--wide').each((i, element) => {
 
-            let result = {
-                title: $(element).find('h3').text(),
-                link: $(element).find('a').attr("href"),
-                summary: $(element).find('p').text().slice(4),
-                img: $(element).find('.inner').attr('style').slice(22, -2)
-            }
-            //this checks if the document exsists in the collection
-            Article.countDocuments({ title: result.title }, (err, test) => {
-                if (test === 0) {
-                    Article.create(result).then((res) => { console.log(res) }).catch((err) => { console.error(err) })
+                let result = {
+                    title: $(element).find('h3').text(),
+                    link: $(element).find('a').attr("href"),
+                    summary: $(element).find('p').text().slice(4),
+                    img: $(element).find('.inner').attr('style').slice(22, -2)
                 }
+                //this checks if the document exsists in the collection
+                Article.countDocuments({ title: result.title }, (err, test) => {
+                    if (test === 0) {
+                        Article.create(result)
+                            .then((res) => {
+                                console.log(res)
+                                // let documents = { article: rese }
+                                // res.render('index', documents)
+                            })
+                            .catch((err) => { console.error(err) })
+                    }
+                })
             })
+            // res.redirect('/')
+            // res.redirect('http://localhost:8080/')
+            // res.redirect('/articles')
+            // res.redirect('http://localhost:8080/')
+            // res.redirect('/')
+            // window.location.reload()
+            // res.redirect('/articles')
+            // res.redirect('back');
         })
-        res.redirect('/')
-    }).catch((err) => {
-        console.error(err)
-    })
+        .catch((err) => {
+            console.error(err)
+        })
+    // res.redirect('/')
 })
 
 //grab every artivle and populate the dom 
@@ -59,6 +74,18 @@ router.get('/articles-json', (req, res) => {
         })
 })
 
+//Delete route to ger rid of a comment 
+router.get('/delete_comment/:id', (req, res) => {
+    Comment.deleteOne({ _id: req.params.id })
+        .then(() => {
+            console.log('removed comment')
+            res.redirect('/')
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})
+
 //route to remove all articles
 router.get('/clear', (req, res) => {
     Article.deleteMany({})
@@ -70,6 +97,7 @@ router.get('/clear', (req, res) => {
             console.error(err)
         })
 })
+
 
 //post route to associate comment with article 
 router.post('/comment/:id', (req, res) => {
@@ -85,11 +113,15 @@ router.post('/comment/:id', (req, res) => {
             return Article.findOneAndUpdate({ _id: articleId }, { $push: { comment: dbComment._id } }, { new: true })
         })
         .then((dbArticle) => {
-            console.log(dbArticle)
+            // console.log(dbArticle)
+            res.redirect('/')
         })
         .catch((err) => {
             console.error(err)
         })
 })
+
+
+
 // Export routes for server.js to use.
 module.exports = router;
